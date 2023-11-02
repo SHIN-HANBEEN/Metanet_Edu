5300278d58cd4240e46dfe8cdb7c5de288208384
SHIN-HANBEEN <shb0801@naver.com>
2023-11-02 22:04:02 +0900

    11.02 10PM offwork commit

---
+++ JAVA_BASIC_SCOPE/src/StringParse.java
@@ -0,0 +1,7 @@
+public class StringParse {
+    public static void main(String[] args) {
+        String s1 = Integer.toString(100);
+        String s2 = String.valueOf(100);
+        System.out.println(s1);
+    }
+}
